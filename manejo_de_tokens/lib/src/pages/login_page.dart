4b1f66d6a61450e130f142303b7a5850ecf4a275
Norberto Paloma <luis_norberto1998@hotmail.com>
2020-06-03 22:13:23 -0500

    Usuario Provider - Login - Verificar usuario y contraseña

--- manejo_de_tokens/lib/src/pages/login_page.dart
+++ manejo_de_tokens/lib/src/pages/login_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:formvalidation/src/blocs/provider.dart';
+import 'package:formvalidation/src/providers/usuario_provider.dart';
 
 class LoginPage extends StatelessWidget {
+  final usuarioProvider = new UsuarioProvider();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -195,11 +197,8 @@ class LoginPage extends StatelessWidget {
   }
 
   _login(LoginBloc bloc, BuildContext context) {
-    print('=============================================');
-    print('Email: ${bloc.email}');
-    print('Password: ${bloc.password}');
-    print('=============================================');
 
-    Navigator.pushReplacementNamed(context, 'home');
+    usuarioProvider.login(bloc.email, bloc.password);
+    // Navigator.pushReplacementNamed(context, 'home');
   }
 }
--- manejo_de_tokens/lib/src/providers/usuario_provider.dart
+++ manejo_de_tokens/lib/src/providers/usuario_provider.dart
@@ -4,8 +4,34 @@ import 'dart:convert';
 class UsuarioProvider {
   final _firebaseToken = 'AIzaSyB6txCnbhbp4F8dGPCsnDucoHTt3bkOPF8';
 
+  //Inicio de sesión
+  Future<Map<String, dynamic>> login(String email, String password) async {
+    final authData = {
+      'email': email,
+      'password': password,
+      'returnSecureToken': true,
+    };
+
+    final resp = await http.post(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=$_firebaseToken',
+      body: json.encode(authData),
+    );
+
+    Map<String, dynamic> decodeResp = json.decode(resp.body);
+
+    print(decodeResp);
+
+    if (decodeResp.containsKey('idToken')) {
+      //TODO: Salvar el token
+      return {'ok': true, 'token': decodeResp['idToken']};
+    } else {
+      return {'ok': true, 'mensaje': decodeResp['error']['menssage']};
+    }
+  }
+
   //Nuevo usuario
-  Future nuevoUsuario(String email, String password) async {
+  Future<Map<String, dynamic>> nuevoUsuario(
+      String email, String password) async {
     final authData = {
       'email': email,
       'password': password,
@@ -19,13 +45,13 @@ class UsuarioProvider {
 
     Map<String, dynamic> decodeResp = json.decode(resp.body);
 
-    // print(decodeResp);
+    print(decodeResp);
 
     if (decodeResp.containsKey('idToken')) {
       //TODO: Salvar el token
       return {'ok': true, 'token': decodeResp['idToken']};
     } else {
-      return {'ok': true, 'mensaje':decodeResp['error']['menssage']};
+      return {'ok': true, 'mensaje': decodeResp['error']['menssage']};
     }
   }
 }
