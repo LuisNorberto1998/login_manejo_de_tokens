e5219e300685acfc2784354738cf51e373b327b5
Norberto Paloma <luis_norberto1998@hotmail.com>
2020-06-24 16:02:06 -0500

    Utilizando productosBloc

--- manejo_de_tokens/lib/src/pages/home_page.dart
+++ manejo_de_tokens/lib/src/pages/home_page.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:formvalidation/src/blocs/provider.dart';
 import 'package:formvalidation/src/models/producto_model.dart';
-import 'package:formvalidation/src/providers/productos_providers.dart';
 
 class HomePage extends StatelessWidget {
-  final productosProvider = new ProductosProvider();
-
   @override
   Widget build(BuildContext context) {
-    final bloc = Provider.of(context);
+    final productosBloc = Provider.productosBloc(context);
+    productosBloc.cargarProductos();
+
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text('Home Page')),
       ),
-      body: _crearListado(),
+      body: _crearListado(productosBloc),
       floatingActionButton: _crearBoton(context),
     );
   }
@@ -26,16 +25,17 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget _crearListado() {
-    return FutureBuilder(
-      future: productosProvider.cargarProductos(),
+  Widget _crearListado(ProductosBloc productosBloc) {
+    return StreamBuilder(
+      stream: productosBloc.productosStream,
       builder:
           (BuildContext context, AsyncSnapshot<List<ProductoModel>> snapshot) {
         if (snapshot.hasData) {
           final productos = snapshot.data;
           return ListView.builder(
             itemCount: productos.length,
-            itemBuilder: (context, i) => _crearItem(context, productos[i]),
+            itemBuilder: (context, i) =>
+                _crearItem(context, productos[i], productosBloc),
           );
         } else {
           return Center(child: CircularProgressIndicator());
@@ -44,15 +44,14 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget _crearItem(BuildContext context, ProductoModel producto) {
+  Widget _crearItem(BuildContext context, ProductoModel producto,
+      ProductosBloc productosBloc) {
     return Dismissible(
       key: UniqueKey(),
       background: Container(
         color: Colors.red,
       ),
-      onDismissed: (direccion) {
-        productosProvider.borrarProducto(producto.id);
-      },
+      onDismissed: (direccion) => productosBloc.borrarProducto(producto.id),
       child: Card(
         child: Column(
           children: <Widget>[
@@ -70,8 +69,8 @@ class HomePage extends StatelessWidget {
             ListTile(
               title: Text('${producto.titulo} - ${producto.valor}'),
               subtitle: Text(producto.id),
-              onTap: () => Navigator.pushNamed(context, 'producto',
-                  arguments: producto),
+              onTap: () =>
+                  Navigator.pushNamed(context, 'producto', arguments: producto),
             ),
           ],
         ),
